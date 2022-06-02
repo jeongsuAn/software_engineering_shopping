c7ca9999b9c201a0e192f8104e55315f5929b105
zero0205 <etety39@naver.com>
2022-06-02 15:53:36 +0900

    Create transferSatisfaction function & Modify comments

--- Satisfaction.h
+++ Satisfaction.h
@@ -1,8 +1,13 @@
 // Class : Satisfaction
 // Description: This is a Satisfaction control class.
 // Created: 2022/5/30 10:00 pm
-// Author: B711089 백지연
+// Author: B711089 Baek Jiyeon
 // mail: etety39@g.hongik.ac.kr
+
+// Revision
+// 1. When & Who : 2022/06/02 15:20 pm by B711089 Baek Jiyeon
+ //      What : added function transferSatisfaction
+
 
 #pragma once
 
@@ -17,5 +22,5 @@ class Satisfaction
 
 public:
     Satisfaction(User *u, FILE *inFp, FILE *outFp);
-    // void transferSatisfaction(Product p);
+    void transferSatisfaction(User *u, std::string pName, std::string score);
 };
